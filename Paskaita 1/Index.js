0594497099e893ad39db0c278dc8749893439568
PLIUSAS <Laurynas1297@gmail.com>
2023-07-24 20:05:56 +0300

    first commit

---
+++ Paskaita 1/Index.js
@@ -0,0 +1,34 @@
+// console.log(); - log
+
+const name  = "lapas";
+let age = 26;
+let lastName;
+console.log(lastName);
+console.log(age);
+age = 30;
+console.log(age);
+
+console.log(name);
+
+const sum = 10 + 15;
+console.log(sum);
+
+const sub = 10 - 7;
+console.log(sub);
+
+const mult = 3 * 8;
+console.log(mult);
+
+const div = 6 / 2;
+console.log(div);
+
+const sum1 = "labas" + "Ate" + "klevasnupustas"
+console.log(sum1);
+
+const sub1 = "labas" * "Ate"
+console.log(sub1);
+
+const sub3 = age + 7;
+console.log(sub3);
+console.log(age);
+console.log(6 + 8);
---
+++ Paskaita 1/Užduotis 1/Index.js
@@ -0,0 +1,26 @@
+// ------- Užduotis 1
+
+// console.log("Hello World!");
+// alert("I love JavaScript!");
+// alert("Ready to learn more?");
+// const number = prompt("Ivesk skaiciu nuo 1 iki 10");
+// console.log(number);
+
+
+
+
+// ------- Užduotis 2
+// const name = prompt("Įveskite savo vardą");
+// const birthDay = "Geguzes 12d.";
+
+// const fullText = name + " yra gimes " + birthDay;
+// console.log(fullText);
+
+
+// ------- Užduotis 3
+
+const bazinisUzmokestis = prompt(" Iveskite bazini uzmokesti ");
+const workingPeople = 1761463;
+const budget = 8487300000;
+const precent = (workingPeople * bazinisUzmokestis * 100) / budget;
+document.write(precent);
