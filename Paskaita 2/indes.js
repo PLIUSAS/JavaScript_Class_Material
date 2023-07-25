6753ccf1c3a1c3d9bfd925b0e2343e85030e2017
PLIUSAS <Laurynas1297@gmail.com>
2023-07-25 19:53:04 +0300

    Paskaita 2

---
+++ Paskaita 2/indes.js
@@ -0,0 +1,62 @@
+console.log("External js file");
+
+const isRainingOutside = true;
+let vardas = null;
+console.log(vardas);
+const didelisskaicius = 1755555555555555555555555555555555555555555555555555n;
+const simbolid = "k";
+
+const isGreater = 2 < 3;
+console.log(isGreater);
+
+const inf = "infinity";
+console.log(inf);
+const lastName ='Pavarde';
+const templateLiteral = `labas ${lastName} ate ${lastName}`;
+console.log(templateLiteral);
+
+const kabutesKabutese = 'Marius sake "labas"';
+console.log(kabutesKabutese);
+
+const kabutesKabutese2 = "Marius sake \"labas\" ";
+console.log(kabutesKabutese2);
+
+console.log(typeof 9);
+
+let number = 9;
+
+number = ++number;
+console.log(number);
+number += 6;
+
+console.log(number);
+
+const array = [1, 2, 'labas', true];
+console.log(array[0]);
+
+const comparison = 4 === "4";
+const comparison1 = 4 !== 4;
+const comparison2 = 4 >= 4;
+console.log(comparison);
+console.log(comparison1);
+console.log(comparison2);
+
+console.log(12 % 5);
+console.log(5 ** 3);
+
+const logic = true && false;
+console.log(logic);
+
+const logic1 = true || false;
+console.log(logic1);
+
+const logic2 = !true;
+console.log(logic2); 
+
+const logic3 = !"cat";
+console.log(logic3);
+
+const ternary = 12 < 5 ? 'Teisinga' : "Neteisinga" ;
+const ternary2 = lastName === "Pavardeasdasd" ?  "Atspejau pavarde" : "Neatspejau pavardes";
+console.log(ternary2);
+console.log(ternary);
