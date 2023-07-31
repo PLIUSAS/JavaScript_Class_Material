1c76fbf6447ebc43b4ba55ec2cd4dee83d06a8da
PLIUSAS <Laurynas1297@gmail.com>
2023-07-31 17:05:52 +0300

    penktadienis

--- Penktadienis/1/index.js
+++ Penktadienis/1/index.js
@@ -90,3 +90,92 @@ for (let index = 0; index < lowercaseWord.length; index++) {
   }
 }
 console.log(totalVowels);
+
+const userInput = "Obuolys";
+switch (userInput) {
+  case "Bulvė":
+  case "Kopūstas":
+  case "Česnakas":
+  case "Svogūnas":
+  case "Morka":
+    console.log("Daržovė");
+    break;
+  case "Obuolys":
+  case "Kriaušė":
+  case "Avietė":
+  case "Braškė":
+  case "Vynuogė":
+    console.log("Vaisius");
+    break;
+  default:
+    console.log("Nei daržovė, nei vaisius");
+}
+let weekDay = new Date().getDay();
+switch (weekDay) {
+  case 0:
+    weekDay = "Sekmadienis";
+    break;
+  case 1:
+    weekDay = "Pirmadienis";
+    break;
+  case 2:
+    weekDay = "Antradienis";
+    break;
+  case 3:
+    weekDay = "Trečiadienis";
+    break;
+  case 4:
+    weekDay = "Ketvirtadienis";
+    break;
+  case 5:
+    weekDay = "Penktadienis";
+    break;
+  case 6:
+    weekDay = "Šeštadienis";
+    break;
+}
+console.log(weekDay);
+const car = "Audi";
+switch (car) {
+  case "VW":
+  case "Audi":
+  case "Bentley":
+  case "Lamborghini":
+  case "Porsche":
+    console.log("VW group");
+    break;
+  case "BMW":
+  case "Mini":
+  case "Rolls-Royce":
+    console.log("BMW group");
+    break;
+  default:
+    console.log("Nei VW group, nei BMW group");
+}
+// const clientAge = 16;
+// const legalAge = 18;
+// clientAge >= legalAge ? console.log("Can drive") : console.log("Can't drive");
+
+const clientAge = 16;
+const legalAge = 18;
+clientAge < 0 || clientAge > 120
+  ? console.log("Invalid age")
+  : clientAge >= legalAge
+  ? console.log("Can drive")
+  : console.log("Can't drive");
+
+const phone = "iPhone";
+const isIphoneUser = phone === "iPhone";
+console.log(isIphoneUser);
+
+const name = "John";
+const count = 10;
+for (let i = 0; i < count; i++) {
+  console.log(name);
+}
+
+let i = 10;
+while (i > 0) {
+  console.log(i);
+  i--;
+}
