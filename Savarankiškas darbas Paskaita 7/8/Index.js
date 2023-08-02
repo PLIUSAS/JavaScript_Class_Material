e1bac20898b7c10ddebba80e0c803b0fce1e1287
PLIUSAS <Laurynas1297@gmail.com>
2023-08-02 19:52:40 +0300

    pratimai su pagalba

--- Savarankiškas darbas Paskaita 7/8/Index.js
+++ Savarankiškas darbas Paskaita 7/8/Index.js
@@ -105,12 +105,110 @@
 
 // 7 Užduotis
 
+// function drawL(event) {
+//   const size = Number(event.target.value);
+//   const outputElement = document.getElementById("output");
+
+//   let output = "";
+//   for (i = 0; i < size - 1; i++) {
+//     output += "L/C<br>";
+//   }
+//   for (i = 0; i < size; i++) {
+//     output += "L/C";
+//   }
+//   outputElement.innerHTML = output;
+// }
+
+// document.getElementById("size").addEventListener("input", drawL);
+
 // 8 Užduotis
 
+// function drawL(event) {
+//   const size = Number(event.target.value);
+//   const outputElement = document.getElementById("output");
+
+//   if (size < 2) {
+//     outputElement.innerHTML = "C letter size must be at least 2";
+//     return;
+//   }
+//   let output = "";
+//   for (i = 0; i < size; i++) {
+//     output += "C";
+//   }
+//   output += "<br>";
+//   for (i = 0; i < size - 2; i++) {
+//     output += "C<br>";
+//   }
+//   for (i = 1; i < size; i++) {
+//     output += "C";
+//   }
+//   outputElement.innerHTML = output;
+// }
+
+// document.getElementById("size").addEventListener("input", drawL);
+
 // 9 Užduotis
+// function addNameToList(event) {
+//   const name = event.target.value.trim();
+//   const outputElement = document.getElementById("output");
+//   if (name) {
+//     outputElement.innerText += `${name}, `;
+//   }
+// }
+
+// document.getElementById("name").addEventListener("blur", addNameToList);
 
 // 10 Užduotis
 
+// function alertNearestNumber(n1, n2) {
+//   if (Math.abs(100 - n1) > Math.abs(100 - n2)) {
+//     alert(n2);
+//   } else {
+//     alert(n1);
+//   }
+// }
+
+// function handleFormSubmit(event) {
+//   event.preventDefault();
+//   const n1 = Number(document.getElementById("number1").value);
+//   const n2 = Number(document.getElementById("number2").value);
+//   alertNearestNumber(n1, n2);
+// }
+
+// document.querySelector("form").addEventListener("submit", handleFormSubmit);
+
 // 11 Užduotis
 
+// let randomNumber = Math.floor(Math.random() * 5) + 1;
+// console.log(randomNumber);
+
+// function guessNumber(event) {
+//   event.preventDefault();
+//   const guessedNumber = Number(document.getElementById("guess").value);
+//   if (randomNumber === guessedNumber) {
+//     alert("Atspėjai");
+//   } else {
+//     alert("Neatspėjai");
+//   }
+// }
+
+// document.querySelector("form").addEventListener("submit", guessNumber);
+
 // 12 Užduotis
+
+let counter = 0;
+let randomNumber = Math.floor(Math.random() * 5) + 1;
+console.log(randomNumber);
+
+function guessNumber(event) {
+  event.preventDefault();
+  counter++;
+  const guessedNumber = Number(document.getElementById("guess").value);
+  if (randomNumber === guessedNumber) {
+    alert(`Atspėjai iš ${counter} karto`);
+  } else {
+    alert("Neatspėjai");
+  }
+}
+
+document.querySelector("form").addEventListener("submit", guessNumber);
