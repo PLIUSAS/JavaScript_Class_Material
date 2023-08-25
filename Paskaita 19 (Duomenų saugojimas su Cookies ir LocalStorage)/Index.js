a9930c338c301190b045f3e127c3c3e3a98ec78f
PLIUSAS <Laurynas1297@gmail.com>
2023-08-25 17:09:43 +0300

    .

--- Paskaita 19 (Duomenų saugojimas su Cookies ir LocalStorage)/Index.js
+++ Paskaita 19 (Duomenų saugojimas su Cookies ir LocalStorage)/Index.js
@@ -1,9 +1,9 @@
-// document.cookie = "name=Marius; expires=Thu, 01 Jan 2024 00:00:00 GMT";
-
 // SessionStorage - Turi Key ir Value. Egzistuoja tik tam paciam Chrome lanke. Atidarius kita langa jis nebe egzistuoja.
 //LocalStorage - Turi Key ir Value. Egzistuoja visalaika.
 //JSON.Stringify - Konvertuoja į stringa PVZ; Iš Array padaro stringa
 //JSON.parse - Parsina atgal į kažkokį objekta PVZ: Iš stringo padaro  Array
+
+// document.cookie = "name=Marius; expires=Thu, 01 Jan 2024 00:00:00 GMT";
 
 console.log(document.cookie);
 console.log(document.cookie.split("=")[1]);
