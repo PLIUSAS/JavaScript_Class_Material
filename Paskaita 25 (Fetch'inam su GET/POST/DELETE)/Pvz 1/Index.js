4adf7f0e787eb8dab432335532b2be1729b7db5d
PLIUSAS <Laurynas1297@gmail.com>
2023-09-07 13:35:25 +0300

    paskaita 25

--- Paskaita 25 (Fetch'inam su GET/POST/DELETE)/Pvz 1/Index.js
+++ Paskaita 25 (Fetch'inam su GET/POST/DELETE)/Pvz 1/Index.js
@@ -1,7 +1,9 @@
 const HOST = "http://localhost:3000/people";
-
+const HOSTER = "https://sophisticated-humane-dandelion.glitch.me";
 const container = document.querySelector(".container");
 const form = document.querySelector("#form");
+const containertest = document.querySelector(".container-test");
+const producte = document.querySelector(".add-product");
 
 async function fetchPeople() {
   try {
@@ -17,6 +19,55 @@ async function fetchPeople() {
   } catch (error) {
     alert("Something went wrong");
   }
+}
+async function fetchproduct() {
+  try {
+    const response = await fetch(HOSTER);
+    if (response.ok) {
+      const product = await response.json();
+      product.forEach((product) => {
+        generateProductCard(product);
+      });
+    } else {
+      alert("Failed to fetch people");
+    }
+  } catch (error) {
+    alert("Something went wrong");
+  }
+}
+fetchproduct();
+
+function generateProductCard(product) {
+  const card = document.createElement("div");
+  card.classList.add("card");
+  const pImage = document.createElement("img");
+  pImage.src = product.image;
+  const pTitle = document.createElement("p");
+  pTitle.textContent = product.title;
+  const pPrice = document.createElement("p");
+  pPrice.classList.add("price");
+  pPrice.textContent = product.price;
+  const DeleteButton = document.createElement("button");
+  DeleteButton.type = "submit";
+  DeleteButton.textContent = "DELETE";
+  DeleteButton.addEventListener("click", async () => {
+    try {
+      const response = await fetch(HOSTER + `/${product.id}`, {
+        method: "DELETE",
+      });
+      if (response.ok) {
+        // e.target.parentElement.remove(); // Pirmas budas korteles istrynimas rankiniu budu
+        card.remove(); // Lengvesnis budas Kortelės istrynimas kadangi jinai yra sukurta šitoj pačioj funkcijoj.
+      } else {
+        alert("Failed to delete person");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+  card.append(pImage, pTitle, pPrice, DeleteButton);
+  containertest.append(card);
 }
 
 function generatePersonCard(person) {
@@ -49,6 +100,59 @@ function generatePersonCard(person) {
 
   container.append(card);
 }
+
+function addProduct() {
+  const productTable = document.createElement("div");
+  productTable.className = "add-product-table";
+  const Pname = document.createElement("h1");
+  Pname.textContent = "Pridėti prekę";
+  const Pimage = document.createElement("Input");
+  Pimage.placeholder = "image";
+  Pimage.type = "text";
+  Pimage.id = "image";
+  const Ptitle = document.createElement("Input");
+  Ptitle.placeholder = "title";
+  Ptitle.type = "text";
+  Ptitle.id = "title";
+  const Pprice = document.createElement("Input");
+  Pprice.placeholder = "price";
+  Pprice.type = "Number";
+  Price.id = "price";
+
+  const addproduct = createElement("Button");
+  addproduct.textContent = "Add product";
+
+  addproduct.addEventListener("click", async () => {
+    try {
+      const image = Pimage.value;
+      const title = Ptitle.value;
+      const price = Pprice.value;
+      const response = await fetch(HOSTER, {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({
+          image,
+          title,
+          price,
+        }),
+      });
+      if (response.ok) {
+        const product = await response.json();
+        generatePersonCard([image, title, price]);
+      } else {
+        alert("It didn't work");
+      }
+    } catch (error) {
+      alert(error);
+    }
+  });
+
+  producte.append(productTable);
+  productTable.append(Pname, Pimage, Ptitle, Pprice, addproduct);
+}
+addProduct();
 
 function generarteForm() {
   // Name su label ir input
